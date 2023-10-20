986b4f1405894646147fc9f469824205a66fc9d0
farhadghasemi <farhadghasemi.official@gmail.com>
2023-10-20 15:29:45 +0330

    update service InvoiceService farhad ghasemi

---
+++ src/main/java/com/mftplus/storage/model/service/InvoiceService.java
@@ -0,0 +1,38 @@
+package com.mftplus.storage.model.service;
+
+import com.mftplus.storage.model.entity.Invoice;
+import com.mftplus.storage.model.service.impl.ServiceImpl;
+
+import java.util.List;
+
+public class InvoiceService implements ServiceImpl<Invoice,Long> {
+    @Override
+    public Invoice save(Invoice invoice) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Invoice edit(Invoice invoice) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Invoice remove(Long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Invoice> findAll() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Invoice findById(Long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public int getCount() throws Exception {
+        return 0;
+    }
+}
