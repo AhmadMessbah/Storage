94725011ec3eb165105b7d8f3650d13534644627
sara <sarazivary925560@gmail.com>
2023-10-20 12:53:36 +0330

    StuffApi

---
+++ src/main/java/com/mftplus/storage/controller/api/StuffApi.java
@@ -0,0 +1,48 @@
+package com.mftplus.storage.controller.api;
+
+import com.mftplus.storage.model.entity.StuffUnit;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import javax.inject.Inject;
+
+@Path("/Stuff")
+
+public class StuffApi {
+    @Inject
+    private StuffService stuffService;
+    @Inject
+    private StuffUnitService stuffUnitService;
+
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response save(Stuff stuff) {
+
+
+        try {
+
+            return Response.ok().entity(stuffServiceService.save(stuff)).build();
+        } catch (Exception e) {
+            return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
+        }
+    }
+
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response save(StuffUnit stuffunit) {
+        try {
+            return Response.ok().entity(stufUnitService.save(stuffunit)).build();
+
+        } catch (Exception e) {
+            reture Response.status(500).entity("{")
+        }
+    }
+}
