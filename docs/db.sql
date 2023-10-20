a864d8405b526994e7c9989f64123920331a9b54
Messbah.A <messbah.a@gmail.com>
2023-10-20 12:24:58 +0330

    first init - messbah.a

---
+++ docs/db.sql
@@ -0,0 +1,12 @@
+create table user_tbl
+(
+    u_username nvarchar2(20),
+    u_password nvarchar2(20)
+);
+
+create table user_roles
+(
+    u_username nvarchar2(20),
+    role_name nvarchar2(20)
+);
+
