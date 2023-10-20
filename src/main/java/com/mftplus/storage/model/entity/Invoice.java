91d6770ba853353ad04c6f09c8af897d5947bbb8
Messbah.A <messbah.a@gmail.com>
2023-10-20 15:19:48 +0330

    update - messbah.a

---
+++ src/main/java/com/mftplus/storage/model/entity/Invoice.java
@@ -0,0 +1,13 @@
+package com.mftplus.storage.model.entity;
+
+public class Invoice {
+    /*
+    id
+    invoiceId
+    dateTime
+    amount
+    discount
+    pureAmount
+    user
+     */
+}
---
+++ src/main/java/com/mftplus/storage/model/entity/InvoiceItem.java
@@ -0,0 +1,11 @@
+package com.mftplus.storage.model.entity;
+
+public class InvoiceItem {
+    /*
+    id
+    invoice
+    stuff
+    count
+    amount
+     */
+}
