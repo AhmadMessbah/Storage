8861d5c5b18f963725748336b94f85eb47054cbe
AliRad <alixrad77@gmail.com>
2023-10-20 13:12:46 +0330

    UserApi Completed (save-edit-remove-findById-findAll) - AliiRad

--- src/main/java/com/mftplus/storage/controller/api/UserApi.java
+++ src/main/java/com/mftplus/storage/controller/api/UserApi.java
@@ -6,6 +6,7 @@ import com.mftplus.storage.model.service.UserService;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.NoContentException;
 import jakarta.ws.rs.core.Response;
 
 import javax.inject.Inject;
@@ -37,6 +38,49 @@ public class UserApi {
             return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
         }
     }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") Long id){
+        try {
+            return Response.ok().entity(userService.remove(id)).build();
+        } catch (Exception e) {
+            return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findAll() {
+        try {
+            System.out.println("OUTPUT : "+userService.findAll());
+            return Response.ok().entity(userService.findAll()).build();
+        }catch (NoContentException e){
+            return Response.noContent().build();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}") .build();
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public Response findById(@PathParam("id") Long id) {
+        System.out.println("FindById Method in UserApi");
+        System.out.println("Id : "+id);
+        try {
+            return Response.ok().entity(userService.findById(id)).build();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return Response.status(204).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
+
+        }
+    }
+
 
 
 
