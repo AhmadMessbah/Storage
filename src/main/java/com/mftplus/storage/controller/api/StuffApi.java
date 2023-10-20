81da620343065ab8e8565f7aa119390a6d3af351
sara <sarazivary925560@gmail.com>
2023-10-20 13:00:27 +0330

    StuffApi -zivary updated

--- src/main/java/com/mftplus/storage/controller/api/StuffApi.java
+++ src/main/java/com/mftplus/storage/controller/api/StuffApi.java
@@ -1,10 +1,8 @@
 package com.mftplus.storage.controller.api;
 
+import com.mftplus.storage.model.entity.Stuff;
 import com.mftplus.storage.model.entity.StuffUnit;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -27,7 +25,7 @@ public class StuffApi {
 
         try {
 
-            return Response.ok().entity(stuffServiceService.save(stuff)).build();
+            return Response.ok().entity(stuffService.save(stuff)).build();
         } catch (Exception e) {
             return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
         }
@@ -39,10 +37,16 @@ public class StuffApi {
     @Produces(MediaType.APPLICATION_JSON)
     public Response save(StuffUnit stuffunit) {
         try {
-            return Response.ok().entity(stuffUnitService.save(stuffunit)).build();
+            return Response.ok().entity(stuffService.save(stuffunit)).build();
 
         } catch (Exception e) {
-            return Response.status(500).entity("{\"message\":"+e.getMessage()+"\"}").build();
+            return Response.status(500).entity("{\"message\":" + e.getMessage() + "\"}").build();
         }
     }
-}
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response remove(@PathParam("id") Long id) {
+
+    }
