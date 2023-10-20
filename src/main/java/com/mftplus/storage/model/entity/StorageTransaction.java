dee0620d22b7bb36db143d56582a5537a46fb945
melika <melikakhadiv@yahoo.com>
2023-10-20 14:25:57 +0330

    storage transaction entity changed

--- src/main/java/com/mftplus/storage/model/entity/StorageTransaction.java
+++ src/main/java/com/mftplus/storage/model/entity/StorageTransaction.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 
 @NamedQueries({@NamedQuery(name="Storage.FindByStock",query = "select oo.stock from storageTransactionEntity oo where oo.stuff.name =:stuffName"),
         @NamedQuery(name="Storage.FindByEntrance", query = "select oo from storageTransactionEntity oo where oo.entrance =:entrance"),
-        @NamedQuery(name="Storage.FindByExit", query = "select oo from storageTransactionEntity oo where oo.exit=:exit") ,
+        @NamedQuery(name="Storage.FindByExit", query = "select oo from storageTransactionEntity oo where oo.outlet=:exit") ,
         @NamedQuery(name="Storage.FindByTransactionDate", query = "select oo from storageTransactionEntity oo where oo.transactionDate =:transactionDate"),
         @NamedQuery(name="Storage.FindByUserId", query = "select oo from storageTransactionEntity oo where oo.user.id =:userId"),
         @NamedQuery(name="Storage.FindByStuffName", query = "select oo from storageTransactionEntity oo where oo.stuff.name =:stuffName")})
@@ -33,7 +33,7 @@ public class StorageTransaction extends Base {
     private LocalDateTime entrance;
 
     @Column(name = "st_exit")
-    private LocalDateTime exit;
+    private LocalDateTime outlet;
 
     @Column(name = "st_stock")
     private long stock;
