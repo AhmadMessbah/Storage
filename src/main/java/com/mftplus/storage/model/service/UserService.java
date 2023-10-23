aff09dff13713169ca5a6502e40f677e4d62ccd3
mahursh <127805335+mahursh@users.noreply.github.com>
2023-10-24 00:39:38 +0330

    Update UserService.java

--- src/main/java/com/mftplus/storage/model/service/UserService.java
+++ src/main/java/com/mftplus/storage/model/service/UserService.java
@@ -15,25 +15,32 @@ import java.util.List;
 @ApplicationScoped
 
 public class UserService implements ServiceImpl<User, Long>, Serializable {
+
     @PersistenceContext(unitName = "mft")
     private EntityManager entityManager;
 
-    @Override
+    //    -------------------------------------------------------------------------
+
     @Transactional
+    @Override
     public User save(User user) throws Exception {
         entityManager.persist(user);
         return user;
     }
 
-    @Override
+    //    -------------------------------------------------------------------------
+
     @Transactional
+    @Override
     public User edit(User user) throws Exception {
         entityManager.merge(user);
         return user;
     }
 
-    @Override
+    //    -------------------------------------------------------------------------
+
     @Transactional
+    @Override
     public User remove(Long id) throws Exception {
         User user = entityManager.find(User.class, id);
         user.setDeleted(true);
@@ -41,32 +48,49 @@ public class UserService implements ServiceImpl<User, Long>, Serializable {
         return user;
     }
 
+    //    -------------------------------------------------------------------------
+
+    @Transactional
     @Override
     public List<User> findAll() throws Exception {
         Query query = entityManager.createQuery("select oo from userEntity oo");
         return query.getResultList();
     }
 
+    //    -------------------------------------------------------------------------
+
+    @Transactional
     @Override
     public User findById(Long id) throws Exception {
         return entityManager.find(User.class, id);
     }
 
+    //    -------------------------------------------------------------------------
+
+    @Transactional
     @Override
     public int getCount() throws Exception {
         return 0;
     }
 
+    //    -------------------------------------------------------------------------
+
+    @Transactional
     public User FindByUserName(String userName) {
         Query query = entityManager.createNamedQuery("User.FindByUserName");
         query.setParameter("userName", userName);
         return (User) query.getSingleResult();
     }
 
+    //    -------------------------------------------------------------------------
+
+    @Transactional
     public User FindByUserNameAndPassword(String userName, String password) {
         Query query = entityManager.createNamedQuery("User.FindByUserNameAndPassword");
         query.setParameter("userName", userName);
         query.setParameter("password", password);
         return (User) query.getSingleResult();
     }
+    //    -------------------------------------------------------------------------
+
 }
