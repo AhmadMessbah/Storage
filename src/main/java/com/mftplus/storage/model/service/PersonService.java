f9ae711ccf4f4097541fa76f7f6ad1e4b46935da
mahursh <127805335+mahursh@users.noreply.github.com>
2023-10-24 01:10:24 +0330

    Update PersonService.java

--- src/main/java/com/mftplus/storage/model/service/PersonService.java
+++ src/main/java/com/mftplus/storage/model/service/PersonService.java
@@ -15,27 +15,30 @@ import java.util.List;
 @ApplicationScoped
 
 public class PersonService implements ServiceImpl<Person, Long>, Serializable {
+
     @PersistenceContext(unitName = "mft")
     private EntityManager entityManager;
+//    -------------------------------------------------------------------------
 
-    @Override
     @Transactional
+    @Override
     public Person save(Person person) throws Exception {
         entityManager.persist(person);
         return person;
     }
+//    -------------------------------------------------------------------------
 
-    @Override
     @Transactional
-
+    @Override
     public Person edit(Person person) throws Exception {
         entityManager.persist(person);
         return person;
 
     }
+//    -------------------------------------------------------------------------
 
-    @Override
     @Transactional
+    @Override
     public Person remove(Long id) throws Exception {
         Person person = entityManager.find(Person.class, id);
         person.setDeleted(true);
@@ -43,25 +46,33 @@ public class PersonService implements ServiceImpl<Person, Long>, Serializable {
         return person;
 
     }
+//    -------------------------------------------------------------------------
 
+    @Transactional
     @Override
     public List<Person> findAll() throws Exception {
         Query query = entityManager.createQuery("select oo from personEntity oo");
         return query.getResultList();
     }
+//    -------------------------------------------------------------------------
 
+    @Transactional
     @Override
     public Person findById(Long id) throws Exception {
-        {
-            return entityManager.find(Person.class, id);
-        }
-    }
 
+        return entityManager.find(Person.class, id);
+
+    }
+//    -------------------------------------------------------------------------
+
+    @Transactional
     @Override
     public int getCount() throws Exception {
         return 0;
     }
+//    -------------------------------------------------------------------------
 
+    @Transactional
     public Person findByNameFamily(String name, String family) {
         Query query = entityManager.createNamedQuery("Person.FindByNameFamily");
         query.setParameter("name", name);
