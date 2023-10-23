2f970244d035a2ea6a451ea6a06d5e34997af687
mahursh <127805335+mahursh@users.noreply.github.com>
2023-10-24 01:37:03 +0330

    Update GroupService.java

--- src/main/java/com/mftplus/storage/model/service/GroupService.java
+++ src/main/java/com/mftplus/storage/model/service/GroupService.java
@@ -1,51 +1,73 @@
 package com.mftplus.storage.model.service;
 
 import com.mftplus.storage.model.entity.Group;
+import com.mftplus.storage.model.service.impl.ServiceImpl;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 import jakarta.transaction.Transactional;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @ApplicationScoped
-public class GroupService {
+public class GroupService implements ServiceImpl<Group,Integer> , Serializable {
+
     @PersistenceContext(unitName = "mft")
     private EntityManager entityManager;
+    //    -------------------------------------------------------------------------
 
     @Transactional
+    @Override
     public Group save(Group group) throws Exception {
         entityManager.persist(group);
         return group;
     }
+    //    -------------------------------------------------------------------------
 
     @Transactional
+    @Override
     public Group edit(Group group) throws Exception {
         entityManager.merge(group);
         return group;
     }
+    //    -------------------------------------------------------------------------
 
     @Transactional
+    @Override
     public Group remove(Integer id) throws Exception {
         Group group = entityManager.find(Group.class, id);
         group.setDeleted(true);
         entityManager.merge(group);
         return group;
     }
+    //    -------------------------------------------------------------------------
 
     @Transactional
+    @Override
     public List<Group> findAll() {
         Query query = entityManager.createQuery("select oo from groupEntity oo");
         return query.getResultList();
     }
+    //    -------------------------------------------------------------------------
 
     @Transactional
+    @Override
     public Group findById(Integer id) {
         return entityManager.find(Group.class, id);
     }
+    //    -------------------------------------------------------------------------
 
+
+    @Transactional
+    @Override
+    public  int getCount() throws Exception {
+        return 0;
+    }
+    //    -------------------------------------------------------------------------
 
     @Transactional
     public Group findByParentId(int id) {
@@ -54,12 +76,24 @@ public class GroupService {
         return (Group) query.getSingleResult();
 
     }
+    //    -------------------------------------------------------------------------
+
+    @Transactional
+    public List<Group> findByParentTitle(String parentTitle){
+        Query query = entityManager.createNamedQuery("Group.FindByParentTitle");
+        query.setParameter("parentTitle",parentTitle);
+        return query.getResultList();
+    }
+
+    //    -------------------------------------------------------------------------
+
 
     @Transactional
     public List<Group> findRootParents() {
         Query query = entityManager.createNamedQuery("Group.FindRootParents");
         return query.getResultList();
     }
+    //    -------------------------------------------------------------------------
 
     @Transactional
     public List<Integer> findParentsId(int id) {
@@ -72,4 +106,6 @@ public class GroupService {
         }
         return parentsId;
     }
+    //    -------------------------------------------------------------------------
+
 }
