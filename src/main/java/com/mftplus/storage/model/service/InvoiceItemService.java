f22501a33da7c5de1adec3373ad9155c57fec372
mahursh <127805335+mahursh@users.noreply.github.com>
2023-10-24 01:19:25 +0330

    Update InvoiceItemService.java

--- src/main/java/com/mftplus/storage/model/service/InvoiceItemService.java
+++ src/main/java/com/mftplus/storage/model/service/InvoiceItemService.java
@@ -14,25 +14,32 @@ import java.util.List;
 @ApplicationScoped
 
 public class InvoiceItemService implements ServiceImpl<InvoiceItem, Long>, Serializable {
+
     @PersistenceContext(unitName = "mft")
     private EntityManager entityManager;
 
-    @Override
+    //    -------------------------------------------------------------------------
+
     @Transactional
+    @Override
     public InvoiceItem save(InvoiceItem invoiceItem) throws Exception {
         entityManager.persist(invoiceItem);
         return invoiceItem;
     }
 
-    @Override
+    //    -------------------------------------------------------------------------
+
     @Transactional
+    @Override
     public InvoiceItem edit(InvoiceItem invoiceItem) throws Exception {
         entityManager.merge(invoiceItem);
         return invoiceItem;
     }
 
-    @Override
+    //    -------------------------------------------------------------------------
+
     @Transactional
+    @Override
     public InvoiceItem remove(Long id) throws Exception {
         InvoiceItem invoiceItem = entityManager.find(InvoiceItem.class, id);
         invoiceItem.setDeleted(true);
@@ -40,26 +47,46 @@ public class InvoiceItemService implements ServiceImpl<InvoiceItem, Long>, Seria
         return invoiceItem;
     }
 
+    //    -------------------------------------------------------------------------
+
+    @Transactional
     @Override
     public List<InvoiceItem> findAll() throws Exception {
         Query query = entityManager.createQuery("select oo from invoiceItemEntity oo");
         return query.getResultList();
     }
 
+    //    -------------------------------------------------------------------------
+
+    @Transactional
     @Override
     public InvoiceItem findById(Long id) throws Exception {
         return entityManager.find(InvoiceItem.class, id);
 
     }
 
-    public InvoiceItem findByStuffId(Long id) {
-        Query query = entityManager.createNamedQuery("InvoiceItem.FindByStuffId");
-        query.setParameter("stuffId", id);
-        return (InvoiceItem) query.getSingleResult();
-    }
+    //    -------------------------------------------------------------------------
 
+    @Transactional
     @Override
     public int getCount() throws Exception {
         return 0;
     }
+    //    -------------------------------------------------------------------------
+    @Transactional
+    public InvoiceItem findByInvoiceId(Long invoiceId) {
+        Query query = entityManager.createNamedQuery("InvoiceItem.FindByInvoiceId");
+        query.setParameter("invoiceId", invoiceId);
+        return (InvoiceItem) query.getSingleResult();
+    }
+    //    -------------------------------------------------------------------------
+
+    @Transactional
+    public InvoiceItem findByStuffId(Long stuffId) {
+        Query query = entityManager.createNamedQuery("InvoiceItem.FindByStuffId");
+        query.setParameter("stuffId", stuffId);
+        return (InvoiceItem) query.getSingleResult();
+    }
+
+
 }
