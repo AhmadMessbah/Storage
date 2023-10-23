98e404db1486bdfcea9296ad09d31d7dc4a22acc
mahursh <127805335+mahursh@users.noreply.github.com>
2023-10-23 23:11:54 +0330

    Update FeatureValueService.java

--- src/main/java/com/mftplus/storage/model/service/FeatureValueService.java
+++ src/main/java/com/mftplus/storage/model/service/FeatureValueService.java
@@ -14,67 +14,84 @@ import java.util.List;
 @ApplicationScoped
 
 public class FeatureValueService implements ServiceImpl<FeatureValue, Long>, Serializable {
+
     @PersistenceContext(unitName = "mft")
     private EntityManager entityManager;
+//    -------------------------------------------------------------------------
 
-    @Override
     @Transactional
+    @Override
     public FeatureValue save(FeatureValue user) throws Exception {
         entityManager.persist(user);
         return user;
     }
+//    -------------------------------------------------------------------------
 
-    @Override
     @Transactional
+    @Override
     public FeatureValue edit(FeatureValue user) throws Exception {
         entityManager.merge(user);
         return user;
     }
+//    -------------------------------------------------------------------------
 
-    @Override
     @Transactional
+    @Override
     public FeatureValue remove(Long id) throws Exception {
         FeatureValue featureValue = entityManager.find(FeatureValue.class, id);
         featureValue.setDeleted(true);
         entityManager.merge(featureValue);
         return featureValue;
     }
+//    -------------------------------------------------------------------------
 
-    @Override
     @Transactional
+    @Override
     public List<FeatureValue> findAll() throws Exception {
         Query query = entityManager.createQuery("select oo from featureValueEntity oo");
         return query.getResultList();
     }
+//    -------------------------------------------------------------------------
 
-    @Override
     @Transactional
+    @Override
     public FeatureValue findById(Long id) throws Exception {
         return entityManager.find(FeatureValue.class, id);
     }
 
+    //    -------------------------------------------------------------------------
+
+    @Transactional
+    @Override
+    public int getCount() throws Exception {
+        return 0;
+    }
+//    -------------------------------------------------------------------------
+
+    @Transactional
     public FeatureValue findByName(String value) {
         Query query = entityManager.createNamedQuery("FeatureValue.FindByName");
         query.setParameter("value", value);
         return (FeatureValue) query.getSingleResult();
     }
+//    -------------------------------------------------------------------------
 
-    public FeatureValue findByGroupId(Long id) {
+    @Transactional
+    public FeatureValue findByGroupId(int id) {
         Query query = entityManager.createNamedQuery("FeatureValue.FindByGroupId");
         query.setParameter("groupId", id);
         return (FeatureValue) query.getSingleResult();
     }
+//    -------------------------------------------------------------------------
 
+    @Transactional
     public FeatureValue FindByPrentTitle(String groupTitle) {
         Query query = entityManager.createNamedQuery("FeatureValue.FindByGroupTitle");
         query.setParameter("groupTitle", groupTitle);
         return (FeatureValue) query.getSingleResult();
     }
+//    -------------------------------------------------------------------------
 
-    @Override
-    @Transactional
-    public int getCount() throws Exception {
-        return 0;
-    }
+
 }
 
