96efe842d0c55e234967d2d4222cfed98b0ac58c
AliRad <alixrad77@gmail.com>
2023-10-24 16:34:14 +0330

    Implement InvoiceApi Implement InvoiceItemApi  - AliiRad

---
+++ src/main/java/com/mftplus/storage/controller/api/InvoiceApi.java
@@ -0,0 +1,85 @@
+package com.mftplus.storage.controller.api;
+
+import com.mftplus.storage.model.entity.Invoice;
+import com.mftplus.storage.model.service.InvoiceService;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import javax.inject.Inject;
+
+@Path("/invoice")
+public class InvoiceApi {
+    @Inject
+    private InvoiceService invoiceService;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response save(Invoice invoice) throws Exception {
+        try {
+            return Response.ok().entity(invoiceService.save(invoice)).build();
+        } catch (Exception e) {
+            return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
+        }
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response edit(Invoice invoice) throws Exception {
+        try {
+            return Response.ok().entity(invoiceService.edit(invoice)).build();
+        } catch (Exception e) {
+            return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response remove(@PathParam("id") Long id) throws Exception {
+        try {
+            return Response.ok().entity(invoiceService.remove(id)).build();
+        } catch (Exception e) {
+            return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
+        }
+    }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findAll() throws Exception {
+        try {
+            return Response.ok().entity(invoiceService.findAll()).build();
+        } catch (Exception e) {
+            return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
+        }
+    }
+
+    @GET
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findById(@PathParam("id") Long id) throws Exception {
+        try {
+            return Response.ok().entity(invoiceService.findById(id)).build();
+        } catch (Exception e) {
+            return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
+        }
+    }
+
+    @GET
+    @Path("/findByInvoiceId/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findByInvoiceId(@PathParam("id") Long invoiceId){
+        try {
+            return Response.ok().entity(invoiceService.findByInvoiceId(invoiceId)).build();
+        } catch (Exception e) {
+            return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
+        }
+    }
+
+}
---
+++ src/main/java/com/mftplus/storage/controller/api/invoiceItemApi.java
@@ -0,0 +1,75 @@
+package com.mftplus.storage.controller.api;
+
+import com.mftplus.storage.model.entity.Invoice;
+import com.mftplus.storage.model.entity.InvoiceItem;
+import com.mftplus.storage.model.service.InvoiceItemService;
+import com.mftplus.storage.model.service.InvoiceService;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import javax.inject.Inject;
+
+@Path("/invoiceItem")
+public class invoiceItemApi {
+    @Inject
+    private InvoiceItemService invoiceItemService;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response save(InvoiceItem invoiceItem) {
+        try {
+            return Response.ok().entity(invoiceItemService.save(invoiceItem)).build();
+        } catch (Exception e) {
+            return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
+        }
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response edit(InvoiceItem invoiceItem) throws Exception {
+        try {
+            return Response.ok().entity(invoiceItemService.edit(invoiceItem)).build();
+        } catch (Exception e) {
+            return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response remove(@PathParam("id") Long id) throws Exception {
+        try {
+            return Response.ok().entity(invoiceItemService.remove(id)).build();
+        } catch (Exception e) {
+            return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
+        }
+    }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findAll() throws Exception {
+        try {
+            return Response.ok().entity(invoiceItemService.findAll()).build();
+        } catch (Exception e) {
+            return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
+        }
+    }
+
+    @GET
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findById(@PathParam("id") Long id) throws Exception {
+        try {
+            return Response.ok().entity(invoiceItemService.findById(id)).build();
+        } catch (Exception e) {
+            return Response.status(500).entity("{\"message\": \"" + e.getMessage() + "\"}").build();
+        }
+    }
+
+}
