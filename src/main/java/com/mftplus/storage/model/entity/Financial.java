dc2e595d3369b7977fa85a9ac3ace0bf8491cd11
AliRad <alixrad77@gmail.com>
2023-10-30 17:10:18 +0330

    Add query attribute into @NamedQuery annotation in Financial Class - AliiRad

--- src/main/java/com/mftplus/storage/model/entity/Financial.java
+++ src/main/java/com/mftplus/storage/model/entity/Financial.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 @Table(name="financial_tbl")
 
 @NamedQueries({
-//        @NamedQuery(name = "")
+//        @NamedQuery(name = "",query = "")
 })
 public class Financial extends Base {
     @Id
