0dd4757b197fd073dec40762ec08ceeb8557e809
AliRad <alixrad77@gmail.com>
2023-10-30 17:04:48 +0330

    Add slf4j dependency into pom.xml use it into Rest Class - AliiRad

--- src/main/java/com/mftplus/storage/controller/api/Rest.java
+++ src/main/java/com/mftplus/storage/controller/api/Rest.java
@@ -2,8 +2,19 @@ package com.mftplus.storage.controller.api;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
+import jdk.dynalink.linker.support.Lookup;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.XSlf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
 
 
 @ApplicationPath("/rest")
 public class Rest extends Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Rest.class);
+    public Rest() {
+        LOGGER.info("Rest Uri Started");
+    }
 }
